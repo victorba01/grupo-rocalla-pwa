ce781f4b642485c0ca382573509c938fef01fc1c
victorba01 <vbastardo@escolainternacional.org>
2025-05-23 10:43:30 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+
+const CACHE_NAME = "grupo-rocalla-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
